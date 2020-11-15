ca2b136f646292d05e39402d43c817041a3abbb0
Zeynep Sena Yurdadur <56610973+zeynep-sena@users.noreply.github.com>
2020-11-15 04:27:18 +0300

    Update agac_olustur.js

--- js/huffman/agac_olustur.js
+++ js/huffman/agac_olustur.js
@@ -2,10 +2,10 @@ function agac_olustur(yaprak_dugumler){
     
 	var yaprak_dugum_sayisi = yaprak_dugumler.length;
 	
-	var kuyruk = new PriorityQueue({ comparator: function(a, b) { return a.frequency - b.frequency; }});
+	var queue = new PriorityQueue({ comparator: function(a, b) { return a.frequency - b.frequency; }});
 	
 	for(i = 0; i < yaprak_dugum_sayisi; i++){
-		kuyruk.queue(yaprak_dugumler[i]);
+		queue.queue(yaprak_dugumler[i]);
 	}
 
 	var id_sayaci = 1;
@@ -13,19 +13,19 @@ function agac_olustur(yaprak_dugumler){
 	for(i = 0; i < yaprak_dugum_sayisi-1; i++){
 		var yeni_dugum = {
 			"id": "0",
-			"isim": "",
-			"cocuk": [kuyruk.dequeue(), kuyruk.dequeue()],
-			"frekans": null,
+			"name": "",
+			"children": [queue.dequeue(), queue.dequeue()],
+			"frequency": null,
 		};
 
-		yeni_dugum.cocuk[0].id = id_sayaci++;
-		yeni_dugum.cocuk[1].id = id_sayaci++;
+		yeni_dugum.children[0].id = id_sayaci++;
+		yeni_dugum.children[1].id = id_sayaci++;
 		
-		yeni_dugum.frekans = yeni_dugum.cocuk[0].frekans + yeni_dugum.cocuk[1].frekans;
-		yeni_dugum.isim = yeni_dugum.frekans;
-		kuyruk.queue(yeni_dugum);
+		yeni_dugum.frequency = yeni_dugum.children[0].frequency + yeni_dugum.children[1].frequency;
+		//yeni_dugum.isim = yeni_dugum.frequency;
+		queue.queue(yeni_dugum);
 		
 	}
 	
-	return kuyruk.dequeue();
+	return queue.dequeue();
 }
