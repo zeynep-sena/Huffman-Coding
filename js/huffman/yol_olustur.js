38536cbb5ea5d1cda5aac5b7d0c5fd2addc05ef1
Zeynep Sena Yurdadur <56610973+zeynep-sena@users.noreply.github.com>
2020-11-15 04:28:24 +0300

    Update yol_olustur.js

--- js/huffman/yol_olustur.js
+++ js/huffman/yol_olustur.js
@@ -6,12 +6,12 @@ function yol_olustur(agac, yollar, suanki_dugum, dugumun_yolu){
 		return yol_olustur(agac, yollar, agac);
 
 	
-	if(suanki_dugum.cocuk.length == 0){
-		yollar[suanki_dugum.isim] = dugumun_yolu != "" ? dugumun_yolu : "0";
+	if(suanki_dugum.children.length == 0){
+		yollar[suanki_dugum.name] = dugumun_yolu != "" ? dugumun_yolu : "0";
 		return;
 	}
 
 	
-	yol_olustur(agac, yollar, suanki_dugum.cocuk[0], dugumun_yolu+"0");
-	yol_olustur(agac, yollar, suanki_dugum.cocuk[1], dugumun_yolu+"1");
+	yol_olustur(agac, yollar, suanki_dugum.children[0], dugumun_yolu+"0");
+	yol_olustur(agac, yollar, suanki_dugum.children[1], dugumun_yolu+"1");
 }
